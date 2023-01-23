1da05fc05eb2602ba9c34ef1d0e9d368dd2d1037
Gabriel Costa <garccosta@gmail.com>
2023-01-23 20:51:36 -0300

    feature: add modal service and its injection on modal component

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { ModalService } from './../../services/modal.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+  
+  constructor(public modal: ModalService) {
+  }
+
+  ngOnInit(): void {
+  }
 
 }
