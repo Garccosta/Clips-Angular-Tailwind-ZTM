24a474b2f54e3220f7b9012699bdaceaa726876e
Gabriel Costa <garccosta@gmail.com>
2023-01-24 20:48:54 -0300

    refactor: add dynamic tabs rendering

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
+
+  ngAfterContentInit(): void {
+  }
 
 }
