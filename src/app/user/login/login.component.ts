bd538bef842fa6323bb211fdaa11cfb7565b1451
Gabriel Costa <garccosta@gmail.com>
2023-01-30 21:19:41 -0300

    feature: add login logic and validation

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,8 +10,31 @@ export class LoginComponent {
     email: '',
     password: ''
   }
+  showAlert = false;
+  alertMsg = 'Please wait! We are logging you in.';
+  alertColor = 'blue';
+  inSubmission = false;
 
-  login() {
-    console.log(this.credentials);
+  constructor(private auth: AngularFireAuth) { }
+
+  async login() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! We are logging you in.';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+
+   try {
+     await this.auth.signInWithEmailAndPassword(
+       this.credentials.email, this.credentials.password
+     )
+   } catch (error) {
+    this.inSubmission = false;
+    this.alertMsg = 'An unexpected error occurred. Please try again later.'; 
+    this.alertColor = 'red';
+
+    return
+   }
+
+   this.alertMsg = 'Success! You are now logged in.';
   }
 }
