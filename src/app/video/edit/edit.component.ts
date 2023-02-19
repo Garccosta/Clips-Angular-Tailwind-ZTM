777de3250436eaea303cd728a4b6e099676f14c7
“Gabriel <garccosta@gmail.com>
2023-02-19 19:32:14 -0300

    feature: add form group, validator and edit title autofill

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import IClip from 'src/app/models/clip.model';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
@@ -7,7 +9,24 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit, OnDestroy {
-  
+  @Input() activeClip: IClip | null = null 
+
+  clipID = new FormControl('', {
+    nonNullable: true,
+  })
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ],
+    nonNullable: true
+  })
+
+  editForm = new FormGroup({
+    title: this.title,
+    id: this.clipID
+  });
+
   constructor(private modal: ModalService) { }
 
   ngOnInit(): void {
@@ -16,5 +35,14 @@ export class EditComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.modal.unregister('editClip')
+  }
+
+  ngOnChanges(): void {
+    if(!this.activeClip) {
+      return
+    }
+
+    this.clipID.setValue(this.activeClip.docID as string)
+    this.title.setValue(this.activeClip.title)
   }
 }
