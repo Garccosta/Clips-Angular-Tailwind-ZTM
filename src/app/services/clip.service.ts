3e00a9708ec05865b3504df69c1f70cf399696d2
“Gabriel <garccosta@gmail.com>
2023-02-19 18:47:41 -0300

    feature: add getUserClips to clip service

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -1,6 +1,8 @@
-import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection, DocumentReference, QuerySnapshot } from '@angular/fire/compat/firestore';
 import { Injectable } from '@angular/core';
 import IClip from '../models/clip.model';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { map, of, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +11,30 @@ export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>
 
   constructor(
-    private db: AngularFirestore
+    private db: AngularFirestore,
+    private auth: AngularFireAuth
   ) {
     this.clipsCollection = db.collection('clips')
   }
 
   createClip(data: IClip): Promise<DocumentReference<IClip>> {
     return  this.clipsCollection.add(data)
+  }
+
+  getUserClips(){
+    return this.auth.user.pipe(
+      switchMap(user => {
+        if(!user) {
+          return of([])
+        }
+
+        const query = this.clipsCollection.ref.where(
+          'uid', '==', user.uid
+        )
+
+        return query.get()
+      }),
+      map(snapshot =>(snapshot as QuerySnapshot<IClip>).docs)
+    )
   }
 }
